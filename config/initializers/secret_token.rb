862a0e41236e3824694cad7b8190230d62fecb49
avipc <avi@ubuntu.(none)>
2012-07-20 03:49:27 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = '890390d28a16fad10374b53d2f12eefbee09c9be62bceedfffcf5d0554eb96c835bf9431cab646525afb2aa1f43ff634c997bc06825e06c4c7f9b6ec8453e4c3'
